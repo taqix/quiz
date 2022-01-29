1b40e9ca8d7e1c429bd4ad4117a549eaf95648ae
Lemanski-Wiktor <wiktorlem01@gmail.com>
2022-01-29 19:06:55 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -21,48 +21,10 @@ function show_start() {
 function hide_logo() {
   document.getElementById('logo').style.display = 'none'
 }
-function hide() {
-  document.getElementById('logo-small').style.opacity = '0'
-  document.getElementById('info').style.opacity = '0'
-  document.getElementById('start').style.opacity = '0'
-  var button2 = document.getElementById('start')
-  button2.disabled = true
-  document.getElementById('output').style.display = 'flex'
-
-  document.getElementById('start').onclick = function start() {
-    var sec = 3
-    if (sec == 0) {
-      stop()
-    }
-
-    var close = setInterval(function () {
-      document.getElementById('output').innerHTML = sec
-      sec--
-      if (sec === -1) {
-        clearInterval(close)
-      }
-    }, 1000)
-  }
-  wyczyscTablice()
-  posortujPytania()
-  document.getElementById('glowna').style.display = 'block'
-  document.getElementById('glowna').style.opacity = '1'
-  document.getElementById('pyt').innerHTML = posortowanePytania[0].question
-  document.getElementById('cos').innerHTML = posortowanePytania[0].optionA
-  document.getElementById('cos2').innerHTML = posortowanePytania[0].optionB
-  document.getElementById('cos3').innerHTML = posortowanePytania[0].optionC
-  document.getElementById('cos4').innerHTML = posortowanePytania[0].optionD
-  document.getElementById('output').style.fontSize = '4vw'
-  setTimeout(() => {
-    document.getElementById('output').style.display = 'none'
-    document.getElementById('glowna').style.display = 'block'
-    document.getElementById('cos').style.display = 'block'
-    document.getElementById('cos2').style.display = 'block'
-    document.getElementById('cos3').style.display = 'block'
-    document.getElementById('cos4').style.display = 'block'
-    document.getElementById('pyt').style.display = 'block'
-    document.getElementById('wyborOdpowiedzi').style.display = 'block'
-  }, 4000)
+function stop()
+{
+  var audio = new Audio('audio/Milionerzy-main.mp3')
+  audio.stop()
 }
 
 let dobreRadio = 'hejka'
@@ -73,43 +35,43 @@ let wynik = 0
 let licznik = 0
 const questions = [
   {
-    question: 'How many days makes a week ?',
-    optionA: '10 days',
-    optionB: '14 days',
-    optionC: '5 days',
-    optionD: '7 days',
+    question: 'Sąsiedzi Polski to:',
+    optionA: 'Dania, Niemcy, Rosja',
+    optionB: 'Chorwacja, Niemcy, Włochy',
+    optionC: 'Czechy, Słowacja, Austria',
+    optionD: 'Litwa, Ukraina, Czechy',
     correctOption: 'optionD',
   },
   {
-    question: 'many days?',
-    optionA: '10 days',
-    optionB: '14 days',
-    optionC: '5 days',
-    optionD: '7 days',
+    question: 'Związek chemiczny wody to:',
+    optionA: 'CO2',
+    optionB: 'H20',
+    optionC: 'HCL',
+    optionD: 'NAOH',
     correctOption: 'optionB',
   },
   {
-    question: 'How days?',
-    optionA: '10 days',
-    optionB: '14 days',
-    optionC: '5 days',
-    optionD: '7 days',
+    question: 'Wynik 2+2*2 wynosi:',
+    optionA: '4',
+    optionB: '8',
+    optionC: '6',
+    optionD: '2',
     correctOption: 'optionC',
   },
   {
-    question: 'How many days?',
-    optionA: '10 days',
-    optionB: '1 days',
-    optionC: '5 days',
-    optionD: '7 days',
+    question: '1kg to ile gram:',
+    optionA: '1000g',
+    optionB: '100g',
+    optionC: '10g',
+    optionD: '10000g',
     correctOption: 'optionA',
   },
   {
-    question: 'How ays?',
-    optionA: '10 days',
-    optionB: '14 days',
-    optionC: '5 days',
-    optionD: '2 days',
+    question: 'Planeta Ziema znajduje się pomiędzy:',
+    optionA: 'Marsem a Jowiszem',
+    optionB: 'Merkurym a Wenus',
+    optionC: 'Jowiszem a Saturnem',
+    optionD: 'Wenus a Marsem',
     correctOption: 'optionD',
   },
 ]
@@ -126,14 +88,31 @@ function wyczyscTablice() {
   posortowanePytania.splice(0, posortowanePytania.length)
 }
 if (indexik === 0) {
+  var audio = new Audio('audio/Milionerzy-background.mp3')
+  audio.play()
+  document.getElementById("pasek-zmienny").style.display = "none"
+  document.getElementById("glowna").style.display = "none"
+  document.getElementById('output').style.display = "block"
+    var sec = 3
+    var close = setInterval(function () {
+      document.getElementById('output').innerHTML = sec
+      sec--
+      if (sec === -1) {
+        clearInterval(close)
+      }
+    }, 1000)
   wyczyscTablice()
   posortujPytania()
-  document.getElementById('pyt').innerHTML = posortowanePytania[0].question
-
-  document.getElementById('cos').innerHTML = posortowanePytania[0].optionA
-  document.getElementById('cos2').innerHTML = posortowanePytania[0].optionB
-  document.getElementById('cos3').innerHTML = posortowanePytania[0].optionC
-  document.getElementById('cos4').innerHTML = posortowanePytania[0].optionD
+  setTimeout(() =>{
+    document.getElementById("output").style.display = "none"
+    document.getElementById("pasek-zmienny").style.display = "block"
+    document.getElementById("glowna").style.display = "block"
+    document.getElementById('pyt').innerHTML = posortowanePytania[0].question
+    document.getElementById('cos').innerHTML = posortowanePytania[0].optionA
+    document.getElementById('cos2').innerHTML = posortowanePytania[0].optionB
+    document.getElementById('cos3').innerHTML = posortowanePytania[0].optionC
+    document.getElementById('cos4').innerHTML = posortowanePytania[0].optionD
+  },4000)
 }
 function wyborQuizu() {
   document.getElementById('pasek-zmienny').style.display = 'none'
@@ -170,6 +149,7 @@ function wyborQuizu() {
     wynik++
     if (wynik === 5) {
       setTimeout(() => {
+        document.getElementById("pasek-zmienny").style.display = "none"
         document.getElementById('option-modal').style.display = 'block'
         document.getElementById('glowna').style.animationName = 'fadeOut'
         document.getElementById('glowna').style.animationDuration = '1.2s'
@@ -278,7 +258,6 @@ function wyborQuizu() {
       setTimeout(() => {
         document.getElementById('glowna').style.animation = 'none'
         document.getElementById('glowna').style.opacity = '0'
-
         document.getElementById('option-modal').style.opacity = '1'
         button.disabled = false
       }, 1200)
